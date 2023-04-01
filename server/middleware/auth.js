2953cd902dde22d054dda6f162be51791e1664c5
AJSteel01 <abhigyan.209301092@muj.manipal.edu>
2023-04-01 22:11:04 +0530

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+
+//wants to like a post
+//click the like button => auth middleware() will check if its okay or not then it will allow to go to like controller
+const secret ='test';
+const auth = async (req,res,next) =>{
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+
+        let decodedData;
+
+        //for our own webtoken 
+        if(token && isCustomAuth){
+            decodedData = jwt.verify(token, secret);
+
+            req.userId=decodedData?.id;
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth;
